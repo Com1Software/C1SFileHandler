fc58314039efadc11f90479e63c283c547d6f7ed
desktop <information@com1software.com>
2023-01-02 14:55:35 -0500

    test

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 
 package main
 
- import "github.com/Com1Software/C1S"
-    
+ import (
+    "github.com/Com1Software/C1S"
+    "fmt"
+ )
  func main() {
+fmt.Println("Test")
   c1s.Cmd("hey there")
  }
